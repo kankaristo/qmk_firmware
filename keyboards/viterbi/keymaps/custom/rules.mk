b90938dad16bd42959927f6464f5a878abec54c7
Sami Kankaristo <sami@indiumgames.fi>
2018-10-01 01:46:28 +0300

    Move rule changes to custom rules.mk

--- keyboards/viterbi/keymaps/custom/rules.mk
+++ keyboards/viterbi/keymaps/custom/rules.mk
@@ -1,1 +1,11 @@
+# Bootloader
+#     This definition is optional, and if your keyboard supports multiple bootloaders of
+#     different sizes, comment this out, and the correct address will be loaded
+#     automatically (+60). See bootloader.mk for all options.
+BOOTLOADER = qmk-dfu
+
 RGBLIGHT_ENABLE = yes
+
+BOOTMAGIC_ENABLE = yes       # Virtual DIP switch configuration(+1000)
+NKRO_ENABLE = yes            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+UNICODE_ENABLE = yes         # Unicode
