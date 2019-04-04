413fb77748d606bbba5c98b4f04c7d3b1b94654d
Sami Kankaristo <sami@indiumgames.fi>
2019-04-05 01:57:40 +0300

    Custom solution working great, QMK is amazing

--- keyboards/keebio/viterbi/keymaps/custom/config.h
+++ keyboards/keebio/viterbi/keymaps/custom/config.h
@@ -31,4 +31,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
 
-#define TAPPING_TERM 150
+//#define TAPPING_TERM 200
--- keyboards/keebio/viterbi/keymaps/custom/rules.mk
+++ keyboards/keebio/viterbi/keymaps/custom/rules.mk
@@ -8,5 +8,5 @@ RGBLIGHT_ENABLE = yes
 
 BOOTMAGIC_ENABLE = yes       # Virtual DIP switch configuration(+1000)
 NKRO_ENABLE = yes            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-TAP_DANCE_ENABLE = yes       # Enable tap-dance
+#TAP_DANCE_ENABLE = yes       # Enable tap-dance
 UNICODE_ENABLE = yes         # Unicode
